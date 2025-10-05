d2d7f9e0262918bc87a6ca3400d21c0779bb2540
Hammad <email1.hammad@gmail.com>
2025-10-05 08:29:15 +0700

    error unpublish filament fix

--- api/index.php
+++ api/index.php
@@ -1,4 +1,16 @@
 <?php
-phpinfo();
 
-require __DIR__.'/../public/index.php';
+// Vercel needs this
+define('LARAVEL_START', microtime(true));
+
+// Bootstrap Laravel
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Handle the request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
