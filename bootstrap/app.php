932e43ab3adc359ad04ea267d7a0829cc29f5df5
Hammad <email1.hammad@gmail.com>
2025-10-05 08:47:13 +0700

    Force HTTPS and trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
